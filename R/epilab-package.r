8259f8bb0d42189c78643a90cef0d7e94fcf36f7
Gustavo Fernandez Bayon <gfernandez@ubuntu.(none)>
2014-02-24 10:08:18 +0100

    - Version 0.2 released. - It includes basic working functionality for annotation of GenomicRanges objects. - Closes #4.

--- R/epilab-package.r
+++ R/epilab-package.r
@@ -1,8 +1,18 @@
 #'
-#' @name epilab
-#' @title EpiLab - Illumina Methylation Microrray
+#' EpiLab - Illumina Methylation Microrray
+#'
+#' Compilation of data, functions and other stuff intended to be used
+#' inside the EpiLab for the implementation of data processing workflows. Most
+#' of the functions are related to the analysis of Illumina 450k microarrays.
+#' For now, the number of functions and methods is so low that I have decide
+#' to keep everything inside only one package. In the future, this package
+#' might be splitted to take into account the different aspects of the lab
+#' workflows.
+#'
 #' @docType package
 #' @rdname epilab-package
+#' @name epilab
+#' @import methods
 #'
 NULL
 
