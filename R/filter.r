748b9d99af01f922528f5c148f3c7c0a4f1de2d2
Gustavo Fernandez Bayon <gfernandez@ubuntu.(none)>
2014-03-04 17:23:31 +0100

    - Added FilterCommand and execute method for the base class. - Closed #12.

---
+++ R/filter.r
@@ -0,0 +1,26 @@
+#'
+#' FilterCommand
+#'
+#' Abstract class that serves as interface for all the filtering commands.
+#'
+setClass('FilterCommand')
+
+#'
+#' FilterCommand base implementation of execute
+#'
+#' Implementation of the logic that is common to all FilterCommands. For 
+#' example, error management and input control. For now, it only prevents the command from being
+#' executed on an empty object.
+#'
+#' @param command A FilterCommand object.
+#' @param object Any object for filtering.
+#'
+setMethod('execute', c('FilterCommand', 'ANY'),
+          function(command, object) {
+            if(is.null(object) || nrow(object) == 0 || ncol(object) == 0) {
+              stop('Cannot execute annotation command on empty object.')
+            } else {
+              return(object)
+            }
+          })
+
