0e4011632fd6c383ce09954bc4a158b343e53f6a
Gustavo Fernandez Bayon <gfernandez@ubuntu.(none)>
2014-03-05 09:09:11 +0100

    - Added DetPFilterCommand class. - Added Accessors for the class. - Added documentation. - Closes #15.

--- R/filter.r
+++ R/filter.r
@@ -30,7 +30,7 @@ setMethod('execute', c('FilterCommand', 'ANY'),
 #' Abstract class that serves as interface for all the filtering commands that are not lists. Allows
 #' to define the filtering direction.
 #'
-#' @slot byRow 
+#' @slot byRow Indicates the direction of filtering, wheter rows or columns should be removed.
 #'
 setClass('AtomicFilterCommand',
          representation(byRow='logical'),
@@ -40,4 +40,115 @@ setClass('AtomicFilterCommand',
            return(length(object@byRow) == 1)
          }
          )
+
+#'
+#' getByRow generic
+#'
+#' Generic definition of the getByRow getter methods.
+#'
+#' @param object An object that contains a byRow slot.
+#'
+setGeneric('getByRow', function(object) standardGeneric('getByRow'))
+
+#'
+#' AtomicFilterCommand base implementation of getByRow
+#'
+#' Getter method for byRow slot for all AtomicFilterCommand objects.
+#'
+#' @param object An AtomicFilterCommand object.
+#'
+setMethod('getByRow', 'AtomicFilterCommand',
+          function(object) {
+            return(object@byRow)
+          })
+
+#' 
+#' setByRow generic
+#'
+#' Generic definition of the setByRow setter methods.
+#'
+#' @param object An object that contains a byRow slot.
+#' @param value The new byRow value.
+#'
+setGeneric('setByRow<-', function(object, value) standardGeneric('setByRow<-'))
+
+#' 
+#' AtomicFilterCommand base implementation of setByRow
+#'
+#' Getter method for byRow slot for all AtomicFilterCommand objects.
+#'
+#' @param object An AtomicFilterCommand object.
+#' @param value The new byRow value.
+#' @name setByRow
+#'
+setReplaceMethod('setByRow', 'AtomicFilterCommand',
+          function(object, value) {
+            object@byRow <- value
+            validObject(object)
+            return(object)
+          })
+
+#'
+#' DetPFilterCommand
+#'
+#' Abstract class that serves as interface for all the filtering commands that use information from
+#' an additional matrix. In our case, this is usually a matrix of detection p-values, and it helps
+#' us to clean a set according to criteria defined over the input objec and the detection p-values
+#' matrix.
+#'
+#' @slot detectionP The additional matrix used in the filtering criterium.
+#'
+setClass('DetPFilterCommand',
+         representation(detectionP='matrix'),
+         prototype(detectionP=NULL),
+         contains='AtomicFilterCommand'
+         )
+
+#'
+#' getDetectionP generic
+#'
+#' Generic definition of the getDetectionP getter methods.
+#'
+#' @param object An object that contains a detectionP slot.
+#'
+setGeneric('getDetectionP', function(object) standardGeneric('getDetectionP'))
+
+#'
+#' DetPFilterCommand base implementation of getDetectionP
+#'
+#' Getter method for detectionP slot for all DetPFilterCommand objects.
+#'
+#' @param object An DetPFilterCommand object.
+#'
+setMethod('getDetectionP', 'DetPFilterCommand',
+          function(object) {
+            return(object@detectionP)
+          })
+
+#' 
+#' setDetectionP generic
+#'
+#' Generic definition of the setDetectionP setter methods.
+#'
+#' @param object An object that contains a detectionP slot.
+#' @param value The new detectionP value.
+#'
+setGeneric('setDetectionP<-', function(object, value) standardGeneric('setDetectionP<-'))
+
+#' 
+#' DetPFilterCommand base implementation of setDetectionP
+#'
+#' Getter method for detectionP slot for all DetPFilterCommand objects.
+#'
+#' @param object An DetPFilterCommand object.
+#' @param value The new detectionP value.
+#' @name setDetectionP
+#'
+setReplaceMethod('setDetectionP', 'DetPFilterCommand',
+          function(object, value) {
+            object@detectionP <- value
+            validObject(object)
+            return(object)
+          })
+
 
