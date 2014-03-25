e05b5bca84b8954b346dcb358cc0b39defa1ffda
Gustavo Fernandez Bayon <gfernandez@ubuntu.(none)>
2014-03-25 12:27:15 +0100

    - Added functions for computing standardized effect sizes. - Added documentation and tests. - Closes #6.

---
+++ R/effectsizes.r
@@ -0,0 +1,114 @@
+#'
+#' Cliff's Delta
+#'
+#' This function computes Cliff's Delta for two samples. It measures the effect size in a 
+#' non parametric way. It is usually employed when a non-parametric test is employed, and a 
+#' parametric effect size estimation, like Cohen's d, would make no sense. This naive implementation 
+#' is quite computationally expensive, so it should be used with caution, as it could lead to memory
+#' shortage problems.
+#'
+#' @param x First sample
+#' @param y Second sample
+#'
+cliffDelta <- function(x, y) {
+  .Deprecated(msg=paste0('This implementation is very inefficient. As of March, 2014, a package ',
+                         'named effsize has been added to CRAN, including a Cliff Delta efficient ',
+                         'implementation.'))
+
+  if (!is.numeric(x) || !is.numeric(y)) {
+    stop('Input samples should be numeric.')
+  }
+
+  return(mean(rowMeans(sign(outer(x, y, FUN="-")))))
+}
+
+#'
+#' Camblor's AC
+#'
+#' This function computes Camblor's AC, a metric for evaluating the effect size in a non-parametric
+#' way between two samples. It was implemented as a efficient replacement for the Cliff's delta in 
+#' this package. Main difference is that AC is contained in the interval [0, 1] and it amounts to 
+#' the area of the two densities (one for each of the input samples) intersection. So, a value of 1
+#' would indicate that the two distributions are the same, hence there would be no effect size. A
+#' value of 0 would indicate no intersection between distributions, and amount to a greater effect
+#' size. It is an unsigned metric, so no information about the direction of the effect is provided.
+#' 
+#' @param x First sample.
+#' @param y Second sample.
+#' @param n Number of points to use for the densities estimation.
+#'
+camblorAC <- function(x, y, n=512) {
+  
+  if (n < 5) {
+    stop('Number of points for density estimation (n) must be higher or equal to 5')
+  }
+
+  minValue <- min(c(x, y), na.rm=TRUE)
+  maxValue <- max(c(x, y), na.rm=TRUE)
+  densX <- density(x, n=n, from=minValue, to=maxValue, na.rm=TRUE)
+  densY <- density(y, n=n, from=minValue, to=maxValue, na.rm=TRUE)
+  densityMaximum <- pmax(densX$y, densY$y)
+  densityMinimum <- pmin(densX$y, densY$y)
+  delta <- diff(densX$x)[1]
+  areaMax <- sum(delta * densityMaximum)
+  areaMin <- sum(delta * densityMinimum)
+  AC <- areaMin / areaMax 
+
+  if (AC > 1.0) {
+    return(1.0)
+  } else {
+    return(AC)
+  }
+}
+
+#'
+#' Odds Ratio
+#'
+#' This function computes the odds ratio for a 2x2 matrix. It is assumed that the true positive 
+#' element count is located in the main diagonal. If that is not the case, the additional parameter
+#' changeRows can be used to reflect that situation.
+#'
+#' @param x Input 2x2 matrix.
+#' @param changeRows boolean indicating if rows have to be exchanged prior to OR computation.
+#'
+oddsRatio <- function(x, changeRows=FALSE) {
+
+  if (!is.matrix(x) || nrow(x) != 2 || ncol(x) != 2) {
+    stop('Input must be a 2x2 matrix.')
+  }
+
+  if (!is.logical(changeRows) || length(changeRows) != 1) {
+    stop('changeRows parameter must be of length 1.')
+  }
+
+  if (changeRows) {
+    x <- rbind(x[2, ], x[1, ])
+  } 
+  x <- as.numeric(x)
+  return((x[1] * x[4]) / (x[2] * x[3])) 
+}
+
+#'
+#' Odds Ratio for a given level
+#'
+#' It is not uncommon to work with nx2 contingency matrices, and it is quite often interesting to 
+#' be able to compute the odds ratios at the different levels the discrete variable can take. This
+#' function does exactly that. It just collapses a bigger matrix onto a 2x2 matrix, like a binary
+#' variable representing the pertenence of the data to the given level. The it computes the odds
+#' ratio in the traditional way.
+#'
+#' @param bigTable The nx2 input matrix.
+#' @param aLevel The level to be studied. It must be a valid rowname for the matrix.
+#'
+oddsRatioLevel <- function(bigTable, aLevel) {
+
+  if (!is.matrix(bigTable) || ncol(bigTable) != 2 || nrow(bigTable) <= 2) {
+    stop('Input must be a nx2 matrix, with n > 2.')
+  }
+
+  indexLevel <- which(rownames(bigTable) == aLevel)
+  collapsedTable <- rbind(bigTable[indexLevel, ], colSums(bigTable[-indexLevel, ]))
+  return((collapsedTable[1, 1] * collapsedTable[2, 2]) / 
+         (collapsedTable[1, 2] * collapsedTable[2, 1])) 
+}
+
---
+++ tests/testthat/test_effectsizes.r
@@ -0,0 +1,111 @@
+context('Effectsizes tests')
+
+#
+# cliffDelta tests
+#
+
+test_that('cliffDelta works only with numerical samples',
+          {
+            expect_error(cliffDelta(letters[1:10], 1:10))
+            expect_error(cliffDelta(factor(c('A', 'B', 'C')), 1:10))
+          })
+
+test_that('cliffDelta works correctly on example',
+          {
+            expect_equal(cliffDelta(1:10, 2:11), -0.19)
+            expect_equal(cliffDelta(2:11, 1:10), 0.19)
+            expect_equal(cliffDelta(2:11, 2:11), 0)
+          })
+
+#
+# camblorAC tests
+#
+
+test_that('camblorAC works only with numerical samples',
+          {
+            expect_error(camblorAC(letters[1:10], 1:10))
+            expect_error(camblorAC(factor(c('A', 'B', 'C')), 1:10))
+          })
+
+test_that('camblorAC fails with wrong n values',
+          {
+            expect_error(camblorAC(1:10, 1:10, n=4)) 
+            expect_error(camblorAC(1:10, 1:10, n=3)) 
+            expect_error(camblorAC(1:10, 1:10, n=2)) 
+            expect_error(camblorAC(1:10, 1:10, n=1)) 
+            expect_error(camblorAC(1:10, 1:10, n=0)) 
+            expect_error(camblorAC(1:10, 1:10, n=-1)) 
+            expect_error(camblorAC(1:10, 1:10, n=-2)) 
+          })
+
+test_that('camblorAC works correctly on example',
+          {
+            expect_equal(camblorAC(1:10, 1:10), 1.0)
+            expect_equal(camblorAC(-10:-1, 1:10), 0.05304149)
+            expect_equal(camblorAC(1:10, 4:14), 0.5710375, tolerance=1e-7)
+            expect_equal(camblorAC(1:10, 1e7:(1e7 + 100)), 0.0, tolerance=1e-7)
+          })
+
+#
+# oddsRatio tests
+#
+
+mockMatrix <- matrix(c(1:4), ncol=2)
+mockBigMatrix <- matrix(c(1:6), ncol=2)
+mockWideMatrix <- matrix(c(1:6), ncol=3)
+
+test_that('oddsRatio works only with 2x2 matrices',
+          {
+            expect_error(oddsRatio('foobar', TRUE))
+            expect_error(oddsRatio(1:10, FALSE))
+            expect_error(oddsRatio(factor(1:10), TRUE))
+            expect_error(oddsRatio(list(1, 2, 3), FALSE))
+            expect_error(oddsRatio(mockBigMatrix, TRUE))
+          })
+
+test_that('oddsRatio breaks on wrong changeRows values',
+          {
+            expect_error(oddsRatio(mockMatrix, 'foobar'))
+            expect_error(oddsRatio(mockMatrix, 1:10))
+            expect_error(oddsRatio(mockMatrix, c(TRUE, FALSE, TRUE)))
+            expect_error(oddsRatio(mockMatrix, factor(1:3)))
+          })
+
+test_that('oddsRatio works correctly on example',
+          {
+            expect_equal(oddsRatio(mockMatrix, FALSE), 2 / 3)
+            expect_equal(oddsRatio(mockMatrix, TRUE), 1.5)
+          })
+
+#
+# oddsRatioLevel tests
+#
+
+rownames(mockMatrix) <- c('foo', 'bar')
+rownames(mockBigMatrix) <- c('foo', 'bar', 'foobar')
+
+test_that('oddsRatioLevel works only with nx2 matrices',
+          {
+            expect_error(oddsRatioLevel('foobar', 'foo'))
+            expect_error(oddsRatioLevel(1:10, 'foo'))
+            expect_error(oddsRatioLevel(factor(1:10), 'foo'))
+            expect_error(oddsRatioLevel(list(1, 2, 3), 'foo'))
+            expect_error(oddsRatioLevel(mockWideMatrix, 'foo'))
+            expect_error(oddsRatioLevel(mockMatrix, 'foo'))
+          })
+
+test_that('oddsRatioLevel breaks on wrong aLevel values',
+          {
+            expect_error(oddsRatioLevel(mockMatrix, 'foobar'))
+            expect_error(oddsRatioLevel(mockMatrix, 1:10))
+            expect_error(oddsRatioLevel(mockMatrix, c(TRUE, FALSE, TRUE)))
+            expect_error(oddsRatioLevel(mockMatrix, factor(1:3)))
+          })
+
+test_that('oddsRatioLevel works correctly on example',
+          {
+            expect_equal(oddsRatioLevel(mockBigMatrix, 'foo'), 11 / 20)
+            expect_equal(oddsRatioLevel(mockBigMatrix, 'bar'), 1)
+            expect_equal(oddsRatioLevel(mockBigMatrix, 'foobar'), 27 / 18)
+          })
+
