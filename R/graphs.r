c89a4bb72d3f99611a18a53181e3c349c561b599
Gustavo Fernandez Bayon <gfernandez@ubuntu.(none)>
2014-03-26 17:00:01 +0100

    - Added low-level functions for the generation of grouped bar graphs.

---
+++ R/graphs.r
@@ -0,0 +1,56 @@
+
+#'
+#' Bar Group Graph
+#'
+#' This low-level function builds a basic bar graph from an input list. Each element of the list
+#' corresponds to a bar in the graph, which is then segmented according to the factor defined by the
+#' element. 
+#' 
+#' @param dataList A list containing the bar groups. Each of the groups has to be of type vector or
+#' factor. All the groups are then concatenated and converted to factors before generating the 
+#' graph.
+#'
+barGroupGraph <- function(dataList) {
+  .bindNameData <- function(name, aList) {
+    cbind(name, as.character(aList[[name]]))
+  }
+
+  if (!is.list(dataList)) {
+    stop('Input must be a list.')
+  }
+
+  plotData <- as.data.frame(Reduce(rbind, 
+                                   lapply(names(dataList), .bindNameData, aList=dataList)))
+  names(plotData) <- c('group', 'status')
+
+  return(ggplot(plotData, aes(x=group, fill=status)) +
+         geom_bar(position='fill', color='black', width=0.6) +
+         theme_bw() +
+         scale_fill_grey())
+}
+
+#'
+#' Bar Group Graph from matrix
+#'
+#' This low-level function builds a basic bar graph from an input matrix. The columns in the matrix
+#' indicate the groups/bars, and the rows correspond to the different levels of the factor being 
+#' studied. It is similar to the barGroupGraph function, but aimed to be used when the data list is
+#' big.
+#' 
+#' @param dataMatrix A matrix containing the bar group element counts. There is a colunn for each of
+#' the bars/groups and a row for each level of the factor being studied.
+#'
+barGroupGraphFromMatrix <- function(dataMatrix) {
+
+  if (!is.matrix(dataMatrix)) {
+    stop('Input must be a matrix')
+  }
+
+  plotData <- melt(dataMatrix, varnames=c('status', 'group'))
+
+  return(ggplot(plotData, aes(x=group, y=value, fill=status)) +
+         geom_bar(position='fill', color='black', width=0.6, stat='identity') +
+         theme_bw() +
+         scale_fill_grey())
+}
+
---
+++ tests/testthat/test_graphs.r
@@ -0,0 +1,64 @@
+context('Graphs tests')
+
+#
+# barGroupGraph tests
+#
+
+test_that('barGroupGraph fails with wrong dataList',
+          {
+            expect_error(barGroupGraph('foo'))
+            expect_error(barGroupGraph(1:10))
+            expect_error(barGroupGraph(TRUE))
+            expect_error(barGroupGraph(matrix(1:10, ncol=2)))
+          })
+
+test_that('barGroupGraph works correctly on a naive example',
+          {
+            mockDataList <- list(g1=c('A', 'B', 'C'),
+                                 g2=factor(c('A', 'A')),
+                                 g3=c('B', 'C'))
+            mockDataFrame <- data.frame(group=c('g1', 'g1', 'g1', 'g2', 'g2', 'g3', 'g3'),
+                                        status=c('A', 'B', 'C', 'A', 'A', 'B', 'C'))
+            foo <- barGroupGraph(mockDataList)
+            expect_true(is(foo, 'gg'))
+            expect_true(is(foo, 'ggplot'))
+            expect_equal(length(names(foo)), 9)
+            expect_equal(names(foo), c('data', 'layers', 'scales', 'mapping', 'theme', 
+                                       'coordinates', 'facet', 'plot_env', 'labels'))
+            expect_equal(foo$data, mockDataFrame)
+            expect_equal(as.character(foo$mapping$x), 'group')
+            expect_equal(as.character(foo$mapping$fill), 'status')
+            expect_equal(p$scales$scales[[1]]$scale_name, 'grey')
+          })
+
+#
+# barGroupGraphFromMatrix tests
+#
+
+test_that('barGroupGraphFromMatrix fails with wrong matrix',
+          {
+            expect_error(barGroupGraphFromMatrix('foo'))
+            expect_error(barGroupGraphFromMatrix(1:10))
+            expect_error(barGroupGraphFromMatrix(TRUE))
+            expect_error(barGroupGraphFromMatrix(list('a', 1, TRUE)))
+          })
+
+test_that('barGroupGraphFromMatrix works correctly on a naive example',
+          {
+            mockDataMatrix <- matrix(c(1, 1, 1, 2, 0, 0, 0, 1, 1), ncol=3)
+            rownames(mockDataMatrix) <- c('A', 'B', 'C')
+            colnames(mockDataMatrix) <- c('g1', 'g2', 'g3')
+            mockDataFrame <- data.frame(status=c('A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C'),
+                                        group=c('g1', 'g1', 'g1', 'g2', 'g2', 'g2', 'g3', 'g3', 'g3'),
+                                        value=c(1, 1, 1, 2, 0, 0, 0, 1, 1))
+            foo <- barGroupGraphFromMatrix(mockDataMatrix)
+            expect_true(is(foo, 'ggplot'))
+            expect_equal(length(names(foo)), 9)
+            expect_equal(names(foo), c('data', 'layers', 'scales', 'mapping', 'theme', 
+                                       'coordinates', 'facet', 'plot_env', 'labels'))
+            expect_equal(foo$data, mockDataFrame)
+            expect_equal(as.character(foo$mapping$x), 'group')
+            expect_equal(as.character(foo$mapping$fill), 'status')
+            expect_equal(p$scales$scales[[1]]$scale_name, 'grey')
+          })
+
